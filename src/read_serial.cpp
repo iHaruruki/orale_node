27916238f9fbb9fbd922c133e4779043d3c6fd83
HarkiIsono <haruki.isono861@gmail.com>
2025-07-03 20:37:31 +0900

    add: chatGPT

--- src/read_serial.cpp
+++ src/read_serial.cpp
@@ -1,46 +1,46 @@
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
+#include <std_msgs/msg/u_int16.hpp>
 #include <opencv2/opencv.hpp>
-#include <algorithm>
-#include <iostream>
-#include <stdio.h>
-#include <string.h>
 #include <fcntl.h>
-#include <assert.h>
 #include <errno.h>
 #include <termios.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/opencv.hpp>
 #include <bitset>
-#include <sstream>
+#include <vector>
+#include <cstring>
+#include <chrono>
 
 using namespace cv;
-using namespace std;
-
-Point pos(-10,-10);//first position
-Mat image(500, 500, CV_8UC3);//y,x
+using namespace std::chrono_literals;
 
 class SensorReader : public rclcpp::Node {
 public:
-    SensorReader() : Node("sensor_reader"),
-                     serial_port_(-1),
-                     image_(500, 500, CV_8UC3, Scalar(255, 255, 255))
+    SensorReader()
+    : Node("sensor_reader"),
+      serial_port_(-1),
+      image_(500, 500, CV_8UC3, Scalar(255, 255, 255)),
+      countup(0), i(0), oneces(0), x(0), y(0), key(0), byteswaiting(0)
     {
-        // パブリッシャーの初期化
+        // cmd_vel パブリッシャー
         cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
 
-        // タイマーの設定（50msごとに実行）
-        timer_ = this->create_wall_timer(
-            std::chrono::milliseconds(50),
-            std::bind(&SensorReader::timer_callback, this));
+        // センサ値用パブリッシャー
+        sensor1_pub_ = this->create_publisher<std_msgs::msg::UInt16>("sensor1", 10);
+        sensor2_pub_ = this->create_publisher<std_msgs::msg::UInt16>("sensor2", 10);
+        sensor3_pub_ = this->create_publisher<std_msgs::msg::UInt16>("sensor3", 10);
+        sensor4_pub_ = this->create_publisher<std_msgs::msg::UInt16>("sensor4", 10);
+        sensor5_pub_ = this->create_publisher<std_msgs::msg::UInt16>("sensor5", 10);
 
-        // OpenCVウィンドウの作成
+        // 50ms ごとのタイマー
+        timer_ = this->create_wall_timer(
+            50ms, std::bind(&SensorReader::timer_callback, this));
+
+        // OpenCV ウィンドウ
         namedWindow("img", WINDOW_AUTOSIZE);
 
-        // シリアルポートの初期化
+        // シリアル初期化
         init_serial();
     }
 
@@ -53,267 +53,179 @@ public:
     }
 
 private:
-    void init_serial() {
+    void init_serial()
+    {
         struct termios tty;
         serial_port_ = open("/dev/ttyACM0", O_RDWR);
-
-        // Check for errors
         if (serial_port_ < 0) {
             RCLCPP_ERROR(this->get_logger(), "Failed to open serial port: %s", strerror(errno));
             return;
         }
-
-        // Read in existing setttings, and handle any error
         if (tcgetattr(serial_port_, &tty) != 0) {
-            RCLCPP_ERROR(this->get_logger(), "Error %i from tcgetattr: %s", errno, strerror(errno));
+            RCLCPP_ERROR(this->get_logger(), "tcgetattr error %i: %s", errno, strerror(errno));
             return;
         }
-
-        // Set tty settings, also check for error
         cfsetospeed(&tty, B9600);
         cfsetispeed(&tty, B9600);
-
-        tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
-        tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
-        tty.c_cflag |= CS8; // 8 bits per byte (most common)
-        tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
-        tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+        tty.c_cflag &= ~PARENB;
+        tty.c_cflag &= ~CSTOPB;
+        tty.c_cflag |= CS8;
+        tty.c_cflag &= ~CRTSCTS;
+        tty.c_cflag |= CREAD | CLOCAL;
         tty.c_lflag &= ~ICANON;
-        tty.c_lflag &= ~ECHO; // Disable echo
-        tty.c_lflag &= ~ECHOE; // Disable erasure
-        tty.c_lflag &= ~ECHONL; // Disable new-line echo
-        tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
-        tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
-        tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
-        tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+        tty.c_lflag &= ~ECHO;
+        tty.c_lflag &= ~ECHOE;
+        tty.c_lflag &= ~ECHONL;
+        tty.c_lflag &= ~ISIG;
+        tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL);
+        tty.c_oflag &= ~OPOST;
+        tty.c_oflag &= ~ONLCR;
         tty.c_cc[VTIME] = 10;
-        tty.c_cc[VMIN] = 0; // 0 means read doesn't block
-
+        tty.c_cc[VMIN] = 0;
         if (tcsetattr(serial_port_, TCSANOW, &tty) != 0) {
-            RCLCPP_ERROR(this->get_logger(), "Error %i from tcsetattr: %s", errno, strerror(errno));
+            RCLCPP_ERROR(this->get_logger(), "tcsetattr error %i: %s", errno, strerror(errno));
             return;
         }
 
-        Point pos(-10,-10); //first position
-        memset(&bset12, '\0', sizeof(bset1));
-        memset(&bset22, '\0', sizeof(bset2));
-        memset(&bset32, '\0', sizeof(bset3));
-        memset(&bset42, '\0', sizeof(bset4));
-        memset(&bset52, '\0', sizeof(bset5));
-
-        countup = 0;
+        // キャリブレーション用配列の初期化
+        memset(bset12, 0, sizeof(bset12));
+        memset(bset22, 0, sizeof(bset22));
+        memset(bset32, 0, sizeof(bset32));
+        memset(bset42, 0, sizeof(bset42));
+        memset(bset52, 0, sizeof(bset52));
 
         RCLCPP_INFO(this->get_logger(), "Serial port initialized.");
     }
 
-    void timer_callback() {
+    void timer_callback()
+    {
+        // シリアル入力バイト数を取得
+        ioctl(serial_port_, FIONREAD, &byteswaiting);
+        if (byteswaiting > 25) {
+            std::vector<uint8_t> read_buf(byteswaiting);
+            int n = read(serial_port_, read_buf.data(), read_buf.size());
+            if (n <= 0) return;
 
-        ioctl(serial_port_, FIONREAD, &byteswaiting);	//read data from serial
-
-		if (byteswaiting > 25){
-			uint8_t read_buf[byteswaiting + 1];
-		  	memset(&read_buf, '\0', sizeof(read_buf));
-		  	read(serial_port_, &read_buf, sizeof(read_buf));
-
-			//printf("2bsize(%d\n", byteswaiting);
-			//printf("3r_bsz(%ld\n", sizeof(read_buf));
-			//cout << "6r_bBy(" << std::bitset<16>{read_buf[0]} << endl;
-
-			for(i = 0; i < sizeof(read_buf); i++){//find the head point
-				//cout << "|check(" <<  std::bitset<8>{read_buf[i]} << endl;//data check
-				if((read_buf[i] == 0xff)&&(read_buf[i+1] == 0xff)&&(read_buf[i+2] != 0xff)&&(read_buf[i+12] == 0xff)&&(read_buf[i+13] == 0xff)){
-				 	break;
-				 	}
-			 	}
-			i+=2;
-			//printf("7point(%d\n", i);
-			//---------------------------------------------------
-			memset(&g, '\0', sizeof(g));
-			memcpy(&g, &read_buf[i], sizeof(g));
-			//for(int l = 0; l < sizeof(g); l++) cout << "|gdata(" <<  std::bitset<8>{g[l]} << endl;//data check
-
-			memset(&bset1, '\0', sizeof(bset1));//fill in the array
-			memset(&bset2, '\0', sizeof(bset2));
-			memset(&bset3, '\0', sizeof(bset3));
-			memset(&bset4, '\0', sizeof(bset4));
-			memset(&bset5, '\0', sizeof(bset5));
-
-			memcpy(&g, &read_buf[i], sizeof(g));
-			memcpy(&bset1, &g[0], 1);bset1[0] = bset1[0] << 8;memcpy(&bset1, &g[1], 1);//read buff
-			memcpy(&bset2, &g[2], 1);bset2[0] = bset2[0] << 8;memcpy(&bset2, &g[3], 1);
-			memcpy(&bset3, &g[4], 1);bset3[0] = bset3[0] << 8;memcpy(&bset3, &g[5], 1);
-			memcpy(&bset4, &g[6], 1);bset4[0] = bset4[0] << 8;memcpy(&bset4, &g[7], 1);
-			memcpy(&bset5, &g[8], 1);bset5[0] = bset5[0] << 8;memcpy(&bset5, &g[9], 1);
-
-
-
-
-			while(oneces == 0){
-				ioctl(serial_port_, FIONREAD, &byteswaiting);	//read data from serial
-
-				if (byteswaiting > 25){
-					uint8_t read_buf [byteswaiting+1];
-				  	memset(&read_buf, '\0', sizeof(read_buf));
-				  	read(serial_port_, &read_buf, sizeof(read_buf));
-
-					//printf("2bsize(%d\n", byteswaiting);
-					//printf("3r_bsz(%ld\n", sizeof(read_buf));
-					//cout << "6r_bBy(" << std::bitset<16>{read_buf[0]} << endl;
-
-					for(i = 0; i < sizeof(read_buf); i++){//find the head point
-						//cout << "|check(" <<  std::bitset<8>{read_buf[i]} << endl;//data check
-						if((read_buf[i] == 0xff)&&(read_buf[i+1] == 0xff)&&(read_buf[i+2] != 0xff)&&(read_buf[i+12] == 0xff)&&(read_buf[i+13] == 0xff)){
-						 	break;
-						}
-					}
-					i+=2;
-					//printf("7point(%d\n", i);
-					//---------------------------------------------------
-					memset(&g, '\0', sizeof(g));
-					memcpy(&g, &read_buf[i], sizeof(g));
-					//for(int l = 0; l < sizeof(g); l++) cout << "|gdata(" <<  std::bitset<8>{g[l]} << endl;//data check
-
-					memcpy(&bset12, &bset1, 1);
-					memcpy(&bset22, &bset2, 1);
-					memcpy(&bset32, &bset3, 1);
-					memcpy(&bset42, &bset4, 1);
-					memcpy(&bset52, &bset5, 1);
-
-					memset(&bset1, '\0', sizeof(bset1));//fill in the array
-					memset(&bset2, '\0', sizeof(bset2));
-					memset(&bset3, '\0', sizeof(bset3));
-					memset(&bset4, '\0', sizeof(bset4));
-					memset(&bset5, '\0', sizeof(bset5));
-
-					memcpy(&g, &read_buf[i], sizeof(g));
-					memcpy(&bset1, &g[0], 1);bset1[0] = bset1[0] << 8;memcpy(&bset1, &g[1], 1);//read buff
-					memcpy(&bset2, &g[2], 1);bset2[0] = bset2[0] << 8;memcpy(&bset2, &g[3], 1);
-					memcpy(&bset3, &g[4], 1);bset3[0] = bset3[0] << 8;memcpy(&bset3, &g[5], 1);
-					memcpy(&bset4, &g[6], 1);bset4[0] = bset4[0] << 8;memcpy(&bset4, &g[7], 1);
-					memcpy(&bset5, &g[8], 1);bset5[0] = bset5[0] << 8;memcpy(&bset5, &g[9], 1);
-
-
-
-	 				if(min1[0] == 0 && bset1[0] != 0 && (bset1[0]-bset12[0]) >= 0)memcpy(&min1,&bset1,1);
-					if(min2[0] == 0 && bset2[0] != 0 && (bset2[0]-bset22[0]) >= 0)memcpy(&min2,&bset2,1);
-					if(min3[0] == 0 && bset3[0] != 0 && (bset3[0]-bset32[0]) >= 0)memcpy(&min3,&bset3,1);
-					if(min4[0] == 0 && bset4[0] != 0 && (bset4[0]-bset42[0]) >= 0)memcpy(&min4,&bset4,1);
-					if(min5[0] == 0 && bset5[0] != 0 && (bset5[0]-bset52[0]) >= 0)memcpy(&min5,&bset5,1);
-
-
-					if((min1[0] != 0) && (min2[0] != 0) && (min3[0] != 0) && (min4[0] != 0) && (min5[0] != 0)){
-					oneces++;
-
-					break;
-					}
-				}
-			}
-			//cout << "b1data(" << std::dec << (bset1[0]) << endl;
-            RCLCPP_INFO(this->get_logger(), "b1:%u", bset1[0]);
-			//cout << "b2data(" << std::dec << (bset2[0]) << endl;
-            RCLCPP_INFO(this->get_logger(), "b2:%u", bset2[0]);
-			//cout << "b3data(" << std::dec << (bset3[0]) << endl;
-            RCLCPP_INFO(this->get_logger(), "b3:%u", bset3[0]);
-			//cout << "b4data(" << std::dec << (bset4[0]) << endl;
-            RCLCPP_INFO(this->get_logger(), "b4:%u", bset4[0]);
-			//cout << "b5data(" << std::dec << (bset5[0]) << endl;
-            RCLCPP_INFO(this->get_logger(), "b5:%u", bset5[0]);
-
-			int border = 130;
-			if((bset1[0] > border)&&(bset2[0] > border)&&(bset3[0] > border)&&(bset4[0] > border)&&(bset5[0] > border)){
-				image = Scalar(255, 255, 255);
-				uint16_t scor = 710;
-
-				float mh1 = (bset1[0] - min1[0]);float mh12 = (scor - min1[0]);
-				float mh2 = (bset2[0] - min2[0]);float mh22 = (scor - min2[0]);
-				float mh3 = (bset3[0] - min3[0]);float mh32 = (scor - min3[0]);
-				float mh4 = (bset4[0] - min4[0]);float mh42 = (scor - min4[0]);
-				float mh5 = (bset5[0] - min5[0]);float mh52 = (scor - min5[0]);
-
-				float s1 = mh1/mh12;
-				float s2 = mh2/mh22;
-				float s3 = mh3/mh32;
-				float s4 = mh4/mh42;
-				float s5 = mh5/mh52;
-				//printf("%f\n", s1);
-				//std::cout << s5 << "," << s2 << "," << s3 << "," << s4 <<std::endl;
-				circle(image, Point(250,250), (35+(65*s1)), cv::Scalar(255, 0,0),2);//center
-				circle(image, Point(250,125), (35+(65*s2)), cv::Scalar(255, 0,0),2);//12
-				circle(image, Point(250,375), (35+(65*s3)), cv::Scalar(255, 0,0),2);//6
-				circle(image, Point(125,250), (35+(65*s4)), cv::Scalar(255, 0,0),2);//9
-		  		circle(image, Point(375,250), (35+(65*s5)), cv::Scalar(255, 0,0),2);//3
-				int x = (((s1*250)+(s2*250)+(s3*250)+(s4*25)+(s5*475))/(s1+s2+s3+s4+s5));
-				int y = (((s1*250)+(s2*25)+(s3*475)+(s4*250)+(s5*250))/(s1+s2+s3+s4+s5));
-                RCLCPP_INFO(this->get_logger(), "x: %d, y: %d", x, y);
-
-                // 速度
-				geometry_msgs::msg::Twist twist_msg;
-                if(x > 290)
-                {
-                    twist_msg.angular.z = 0.2;
+            // フレームヘッダ探査
+            int idx = 0;
+            for (; idx < n - 13; ++idx) {
+                if (read_buf[idx] == 0xff && read_buf[idx+1] == 0xff &&
+                    read_buf[idx+2] != 0xff &&
+                    read_buf[idx+12] == 0xff && read_buf[idx+13] == 0xff) {
+                    break;
                 }
-                else if(x < 210)
-                {
-                    twist_msg.angular.z = -0.2;
-                }
-                else if(y > 290)
-                {
-                    twist_msg.linear.x = -0.1;
-                }
-                else if(y < 210)
-                {
-                    twist_msg.linear.x = 0.1;
-                }
-                else
-                {
-                    twist_msg.linear.x = 0.0;
-                    twist_msg.angular.z = 0.0;
-                }
-				cmd_vel_pub_->publish(twist_msg);
+            }
+            idx += 2;
 
-				circle(image,Point(x,y), 10, cv::Scalar(0,0,255),-1);
-			//}
-			imshow("img",image);
-			key = waitKey(10);
-			//printf("\n");
-			}
+            // 10 バイトのデータ取得
+            uint8_t g[10];
+            memcpy(g, &read_buf[idx], sizeof(g));
 
-		}
+            // 各センサ値を結合 (big-endian)
+            bset1[0] = (uint16_t(g[0]) << 8) | g[1];
+            bset2[0] = (uint16_t(g[2]) << 8) | g[3];
+            bset3[0] = (uint16_t(g[4]) << 8) | g[5];
+            bset4[0] = (uint16_t(g[6]) << 8) | g[7];
+            bset5[0] = (uint16_t(g[8]) << 8) | g[9];
 
-        void init_serial();
-        void timer_callback();
-        void process_data(uint8_t* data, int size);
-        void update_sensor_values();
-        void calculate_and_publish_velocity();
-        void update_display();
-        int calculate_x_position();
-        float map_to_velocity(int x);
-        void draw_sensor_circles();
+            // 初回のみ最小値キャリブレーション
+            if (oneces == 0) {
+                memcpy(min1, bset1, sizeof(bset1));
+                memcpy(min2, bset2, sizeof(bset2));
+                memcpy(min3, bset3, sizeof(bset3));
+                memcpy(min4, bset4, sizeof(bset4));
+                memcpy(min5, bset5, sizeof(bset5));
+                oneces = 1;
+                RCLCPP_INFO(this->get_logger(), "Calibration done.");
+            }
 
+            // 値のログ出力
+            RCLCPP_INFO(this->get_logger(),
+                        "b1:%u b2:%u b3:%u b4:%u b5:%u",
+                        bset1[0], bset2[0], bset3[0], bset4[0], bset5[0]);
+
+            // 各センサ値を publish
+            std_msgs::msg::UInt16 msg16;
+            msg16.data = bset1[0]; sensor1_pub_->publish(msg16);
+            msg16.data = bset2[0]; sensor2_pub_->publish(msg16);
+            msg16.data = bset3[0]; sensor3_pub_->publish(msg16);
+            msg16.data = bset4[0]; sensor4_pub_->publish(msg16);
+            msg16.data = bset5[0]; sensor5_pub_->publish(msg16);
+
+            // 位置計算＆描画（既存ロジック）
+            int border = 130;
+            if (bset1[0] > border && bset2[0] > border &&
+                bset3[0] > border && bset4[0] > border &&
+                bset5[0] > border)
+            {
+                image_ = Scalar(255,255,255);
+                float scor = 710.0f;
+                float bs[5] = {
+                    float(bset1[0]), float(bset2[0]),
+                    float(bset3[0]), float(bset4[0]),
+                    float(bset5[0])
+                };
+                float mn[5] = {
+                    float(min1[0]), float(min2[0]),
+                    float(min3[0]), float(min4[0]),
+                    float(min5[0])
+                };
+                float s[5];
+                for (int k = 0; k < 5; ++k) {
+                    s[k] = (bs[k] - mn[k]) / (scor - mn[k]);
+                }
+                // 円描画
+                circle(image_, Point(250,250), int(35+65*s[0]), Scalar(255,0,0), 2);
+                circle(image_, Point(250,125), int(35+65*s[1]), Scalar(255,0,0), 2);
+                circle(image_, Point(250,375), int(35+65*s[2]), Scalar(255,0,0), 2);
+                circle(image_, Point(125,250), int(35+65*s[3]), Scalar(255,0,0), 2);
+                circle(image_, Point(375,250), int(35+65*s[4]), Scalar(255,0,0), 2);
+
+                // 重心計算
+                x = int((s[0]*250 + s[1]*250 + s[2]*250 + s[3]*25 + s[4]*475) /
+                        (s[0]+s[1]+s[2]+s[3]+s[4]));
+                y = int((s[0]*250 + s[1]*25  + s[2]*475 + s[3]*250 + s[4]*250) /
+                        (s[0]+s[1]+s[2]+s[3]+s[4]));
+                RCLCPP_INFO(this->get_logger(), "x:%d, y:%d", x, y);
+
+                // Twist publish
+                geometry_msgs::msg::Twist twist;
+                if      (x > 280) twist.angular.z =  0.2f;
+                else if (x < 220) twist.angular.z = -0.2f;
+                else if (y > 280) twist.linear.x  = -0.1f;
+                else if (y < 220) twist.linear.x  =  0.1f;
+                /*else{
+                    twist.linear.x = 0;
+                    twist.angular.z = 0;
+                }*/
+                cmd_vel_pub_->publish(twist);
+
+                // 重心点表示
+                circle(image_, Point(x,y), 10, Scalar(0,0,255), -1);
+                imshow("img", image_);
+                waitKey(1);
+            }
+        }
     }
 
     // メンバ変数
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+    rclcpp::Publisher<std_msgs::msg::UInt16>::SharedPtr
+        sensor1_pub_, sensor2_pub_, sensor3_pub_, sensor4_pub_, sensor5_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
     int serial_port_;
     cv::Mat image_;
     uint8_t byteswaiting;
-
-    int countup;
-    int i, oneces;
-    int x, y;
-    int key;
-
-    uint8_t g[10];
+    int countup, i, oneces, x, y, key;
     uint16_t bset1[1], bset2[1], bset3[1], bset4[1], bset5[1];
     uint16_t bset12[1], bset22[1], bset32[1], bset42[1], bset52[1];
     uint16_t min1[1], min2[1], min3[1], min4[1], min5[1];
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<SensorReader>();
-    rclcpp::spin(node);
+    rclcpp::spin(std::make_shared<SensorReader>());
     rclcpp::shutdown();
     return 0;
 }
