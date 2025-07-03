5fc4daf74fefa5b2450340dcf834c4c6cf90b3b1
HarkiIsono <haruki.isono861@gmail.com>
2025-07-03 16:39:01 +0900

    add

--- src/read_serial.cpp
+++ src/read_serial.cpp
@@ -5,11 +5,11 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
-#include <fcntl.h> 
+#include <fcntl.h>
 #include <assert.h>
-#include <errno.h> 
+#include <errno.h>
 #include <termios.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <sys/ioctl.h>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
@@ -27,12 +27,12 @@ class SensorReader : public rclcpp::Node {
 public:
     SensorReader() : Node("sensor_reader"),
                      serial_port_(-1),
-                     image_(500, 500, CV_8UC3, Scalar(255, 255, 255))   
+                     image_(500, 500, CV_8UC3, Scalar(255, 255, 255))
     {
         // パブリッシャーの初期化
         cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
-        
-        // タイマーの設定（100msごとに実行）
+
+        // タイマーの設定（50msごとに実行）
         timer_ = this->create_wall_timer(
             std::chrono::milliseconds(50),
             std::bind(&SensorReader::timer_callback, this));
@@ -86,7 +86,7 @@ private:
         tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
         tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
         tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
-        tty.c_cc[VTIME] = 10;   
+        tty.c_cc[VTIME] = 10;
         tty.c_cc[VMIN] = 0; // 0 means read doesn't block
 
         if (tcsetattr(serial_port_, TCSANOW, &tty) != 0) {
@@ -107,47 +107,47 @@ private:
     }
 
     void timer_callback() {
-        
+
         ioctl(serial_port_, FIONREAD, &byteswaiting);	//read data from serial
 
 		if (byteswaiting > 25){
 			uint8_t read_buf[byteswaiting + 1];
 		  	memset(&read_buf, '\0', sizeof(read_buf));
 		  	read(serial_port_, &read_buf, sizeof(read_buf));
-							
-			//printf("2bsize(%d\n", byteswaiting);		
+
+			//printf("2bsize(%d\n", byteswaiting);
 			//printf("3r_bsz(%ld\n", sizeof(read_buf));
 			//cout << "6r_bBy(" << std::bitset<16>{read_buf[0]} << endl;
-			
+
 			for(i = 0; i < sizeof(read_buf); i++){//find the head point
 				//cout << "|check(" <<  std::bitset<8>{read_buf[i]} << endl;//data check
 				if((read_buf[i] == 0xff)&&(read_buf[i+1] == 0xff)&&(read_buf[i+2] != 0xff)&&(read_buf[i+12] == 0xff)&&(read_buf[i+13] == 0xff)){
 				 	break;
-				 	} 
+				 	}
 			 	}
-			i+=2;	
+			i+=2;
 			//printf("7point(%d\n", i);
 			//---------------------------------------------------
 			memset(&g, '\0', sizeof(g));
 			memcpy(&g, &read_buf[i], sizeof(g));
 			//for(int l = 0; l < sizeof(g); l++) cout << "|gdata(" <<  std::bitset<8>{g[l]} << endl;//data check
-			
+
 			memset(&bset1, '\0', sizeof(bset1));//fill in the array
 			memset(&bset2, '\0', sizeof(bset2));
 			memset(&bset3, '\0', sizeof(bset3));
 			memset(&bset4, '\0', sizeof(bset4));
 			memset(&bset5, '\0', sizeof(bset5));
-			
+
 			memcpy(&g, &read_buf[i], sizeof(g));
 			memcpy(&bset1, &g[0], 1);bset1[0] = bset1[0] << 8;memcpy(&bset1, &g[1], 1);//read buff
 			memcpy(&bset2, &g[2], 1);bset2[0] = bset2[0] << 8;memcpy(&bset2, &g[3], 1);
 			memcpy(&bset3, &g[4], 1);bset3[0] = bset3[0] << 8;memcpy(&bset3, &g[5], 1);
 			memcpy(&bset4, &g[6], 1);bset4[0] = bset4[0] << 8;memcpy(&bset4, &g[7], 1);
 			memcpy(&bset5, &g[8], 1);bset5[0] = bset5[0] << 8;memcpy(&bset5, &g[9], 1);
-			
-			
-		
-			
+
+
+
+
 			while(oneces == 0){
 				ioctl(serial_port_, FIONREAD, &byteswaiting);	//read data from serial
 
@@ -155,81 +155,83 @@ private:
 					uint8_t read_buf [byteswaiting+1];
 				  	memset(&read_buf, '\0', sizeof(read_buf));
 				  	read(serial_port_, &read_buf, sizeof(read_buf));
-									
-					//printf("2bsize(%d\n", byteswaiting);		
+
+					//printf("2bsize(%d\n", byteswaiting);
 					//printf("3r_bsz(%ld\n", sizeof(read_buf));
 					//cout << "6r_bBy(" << std::bitset<16>{read_buf[0]} << endl;
-					
+
 					for(i = 0; i < sizeof(read_buf); i++){//find the head point
 						//cout << "|check(" <<  std::bitset<8>{read_buf[i]} << endl;//data check
 						if((read_buf[i] == 0xff)&&(read_buf[i+1] == 0xff)&&(read_buf[i+2] != 0xff)&&(read_buf[i+12] == 0xff)&&(read_buf[i+13] == 0xff)){
 						 	break;
-						} 
+						}
 					}
-					i+=2;	
+					i+=2;
 					//printf("7point(%d\n", i);
 					//---------------------------------------------------
 					memset(&g, '\0', sizeof(g));
 					memcpy(&g, &read_buf[i], sizeof(g));
 					//for(int l = 0; l < sizeof(g); l++) cout << "|gdata(" <<  std::bitset<8>{g[l]} << endl;//data check
-					
+
 					memcpy(&bset12, &bset1, 1);
 					memcpy(&bset22, &bset2, 1);
 					memcpy(&bset32, &bset3, 1);
 					memcpy(&bset42, &bset4, 1);
 					memcpy(&bset52, &bset5, 1);
-					
+
 					memset(&bset1, '\0', sizeof(bset1));//fill in the array
 					memset(&bset2, '\0', sizeof(bset2));
 					memset(&bset3, '\0', sizeof(bset3));
 					memset(&bset4, '\0', sizeof(bset4));
 					memset(&bset5, '\0', sizeof(bset5));
-					
+
 					memcpy(&g, &read_buf[i], sizeof(g));
 					memcpy(&bset1, &g[0], 1);bset1[0] = bset1[0] << 8;memcpy(&bset1, &g[1], 1);//read buff
 					memcpy(&bset2, &g[2], 1);bset2[0] = bset2[0] << 8;memcpy(&bset2, &g[3], 1);
 					memcpy(&bset3, &g[4], 1);bset3[0] = bset3[0] << 8;memcpy(&bset3, &g[5], 1);
 					memcpy(&bset4, &g[6], 1);bset4[0] = bset4[0] << 8;memcpy(&bset4, &g[7], 1);
 					memcpy(&bset5, &g[8], 1);bset5[0] = bset5[0] << 8;memcpy(&bset5, &g[9], 1);
-				
-				
-				
+
+
+
 	 				if(min1[0] == 0 && bset1[0] != 0 && (bset1[0]-bset12[0]) >= 0)memcpy(&min1,&bset1,1);
 					if(min2[0] == 0 && bset2[0] != 0 && (bset2[0]-bset22[0]) >= 0)memcpy(&min2,&bset2,1);
 					if(min3[0] == 0 && bset3[0] != 0 && (bset3[0]-bset32[0]) >= 0)memcpy(&min3,&bset3,1);
 					if(min4[0] == 0 && bset4[0] != 0 && (bset4[0]-bset42[0]) >= 0)memcpy(&min4,&bset4,1);
 					if(min5[0] == 0 && bset5[0] != 0 && (bset5[0]-bset52[0]) >= 0)memcpy(&min5,&bset5,1);
 
-					
+
 					if((min1[0] != 0) && (min2[0] != 0) && (min3[0] != 0) && (min4[0] != 0) && (min5[0] != 0)){
 					oneces++;
-					
+
 					break;
 					}
 				}
 			}
-			cout << "b1data(" << std::dec << (bset1[0]) << endl;
-			//cout << "b2data(" << std::bitset<16>{bset2[0]} << endl;
-			cout << "b2data(" << std::dec << (bset2[0]) << endl;
-	   		//cout << "b3data(" << std::bitset<16>{bset3[0]} << endl;
-			cout << "b3data(" << std::dec << (bset3[0]) << endl;
-			//cout << "b4data(" << std::bitset<16>{bset4[0]} << endl;
-			cout << "b4data(" << std::dec << (bset4[0]) << endl;
-			//cout << "b5data(" << std::bitset<16>{bset5[0]} << endl;
-			cout << "b5data(" << std::dec << (bset5[0]) << endl;		
+			//cout << "b1data(" << std::dec << (bset1[0]) << endl;
+            RCLCPP_INFO(this->get_logger(), "b1:%d", bset1[0]);
+			//cout << "b2data(" << std::dec << (bset2[0]) << endl;
+            RCLCPP_INFO(this->get_logger(), "b2:%d", bset2[0]);
+			//cout << "b3data(" << std::dec << (bset3[0]) << endl;
+            RCLCPP_INFO(this->get_logger(), "b3:%d", bset3[0]);
+			//cout << "b4data(" << std::dec << (bset4[0]) << endl;
+            RCLCPP_INFO(this->get_logger(), "b4:%d", bset4[0]);
+			//cout << "b5data(" << std::dec << (bset5[0]) << endl;
+            RCLCPP_INFO(this->get_logger(), "b5:%d", bset5[0]);
+
 			int border = 130;
 			if((bset1[0] > border)&&(bset2[0] > border)&&(bset3[0] > border)&&(bset4[0] > border)&&(bset5[0] > border)){
 				image = Scalar(255, 255, 255);
 				uint16_t scor = 710;
-			
+
 				float mh1 = (bset1[0] - min1[0]);float mh12 = (scor - min1[0]);
 				float mh2 = (bset2[0] - min2[0]);float mh22 = (scor - min2[0]);
 				float mh3 = (bset3[0] - min3[0]);float mh32 = (scor - min3[0]);
 				float mh4 = (bset4[0] - min4[0]);float mh42 = (scor - min4[0]);
 				float mh5 = (bset5[0] - min5[0]);float mh52 = (scor - min5[0]);
-				
+
 				float s1 = mh1/mh12;
-				float s2 = mh2/mh22; 
+				float s2 = mh2/mh22;
 				float s3 = mh3/mh32;
 				float s4 = mh4/mh42;
 				float s5 = mh5/mh52;
@@ -269,14 +271,14 @@ private:
                     twist_msg.angular.z = 0.0;
                 }
 				cmd_vel_pub_->publish(twist_msg);
-				
+
 				circle(image,Point(x,y), 10, cv::Scalar(0,0,255),-1);
 			//}
 			imshow("img",image);
 			key = waitKey(10);
 			//printf("\n");
 			}
-			
+
 		}
 
         void init_serial();
@@ -302,7 +304,7 @@ private:
     int i, oneces;
     int x, y;
     int key;
-    
+
     uint8_t g[10];
     uint16_t bset1[1], bset2[1], bset3[1], bset4[1], bset5[1];
     uint16_t bset12[1], bset22[1], bset32[1], bset42[1], bset52[1];
